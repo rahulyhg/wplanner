109fe50e82ee7b9adac7a43de62faa83fb9aa526
Susana <sue.gutierrez@opendeusto.es>
2014-12-03 01:22:14 +0100

    Actualizaciones de clases a jdo

---
+++ WPlanner/src/es/deusto/ingenieria/p3/jdo/Categoria.java
@@ -0,0 +1,12 @@
+package es.deusto.ingenieria.p3.jdo;
+
+/** Clase de enumerados que define la categoría posible
+ * correspondiente a la tarea.
+ * @author Sue
+ */
+public enum Categoria {
+	Musica, Banquete, Planificacion, Fotografia, Ceremonia, Tramites,
+	Invitaciones, Detalles, Flores, Transportes, Joyeria, Belleza, LunaMiel, 
+	ListaBoda, Traje, Vestido, Complementos, Otros, Varios
+ 
+}
